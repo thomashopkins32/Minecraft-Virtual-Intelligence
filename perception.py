4d2c5633809469a80e0516821d3530d36bd08b96
Thomas Hopkins <thomashopkins000@gmail.com>
2023-10-15 19:13:40 -0400

    First commit

---
+++ perception.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+
+class Vision(nn.Module):
+    ''' Module for perceptual vision '''
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self, x):
+        pass
