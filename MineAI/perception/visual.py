f6dc5c26571822fd40f268e41b35d0fed1014081
Thomas Hopkins <thomashopkins000@gmail.com>
2024-01-22 08:34:15 -0500

    Added more tests; Refactored to use MultiHeadAttention

--- MineAI/perception/visual.py
+++ MineAI/perception/visual.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
-from torchvision.transforms.functional import center_crop
-
-from utils import compute_kernel_size, compute_stride
+from torchvision.transforms.functional import center_crop, rgb_to_grayscale
 
 
 class VisualPerception(nn.Module):
@@ -20,15 +18,17 @@ class VisualPerception(nn.Module):
         - The region of interest shall be determined by the most recent output of the actor module
     """
 
-    def __init__(self, output_shape=(32, 8, 8), roi_shape=(16, 16)):
+    def __init__(self, out_channels=32, roi_shape=(32, 32)):
         super().__init__()
         # Set region of interest height and width
         self.roi_shape = roi_shape
 
         # Set up sub-modules
-        self.foveated_perception = FoveatedPerception(output_shape)
-        self.peripheral_perception = PeripheralPerception(output_shape)
-        self.combiner = VisualAttention(output_shape[0])
+        self.foveated_perception = FoveatedPerception(3, out_channels)
+        self.peripheral_perception = PeripheralPerception(1, out_channels)
+
+        # Combiner
+        self.attention = nn.MultiheadAttention(128, 3, batch_first=True)
 
     def forward(self, x_img: torch.Tensor, x_roi: torch.Tensor = None) -> torch.Tensor:
         """
@@ -55,13 +55,16 @@ class VisualPerception(nn.Module):
                 self.roi_shape[0],
                 self.roi_shape[1],
             )
+        gray_x = rgb_to_grayscale(x_img)
 
         fov_x = self.foveated_perception(x_roi)
-        per_x = self.peripheral_perception(x_img)
+        per_x = self.peripheral_perception(gray_x)
 
-        combined = self.combiner(fov_x, per_x)
+        combined = torch.cat((fov_x, per_x), dim=1)
+        combined = combined.view(combined.size(0), 1, -1)
+        out = self.attention(combined, combined, combined)
 
-        return combined
+        return out
 
 
 class FoveatedPerception(nn.Module):
@@ -73,27 +76,18 @@ class FoveatedPerception(nn.Module):
     It does so by using small convolutions with low stride.
     """
 
-    def __init__(self, input_shape, output_shape):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv1 = nn.Conv2d(
-            input_shape[0], output_shape[0] // 2, kernel_size=3, stride=1, padding=1
+            in_channels, out_channels // 2, kernel_size=3, stride=1, padding=1
         )
         self.conv2 = nn.Conv2d(
-            output_shape[0] // 2, output_shape[0], kernel_size=3, stride=1, padding=1
+            out_channels // 2, out_channels, kernel_size=3, stride=1, padding=1
         )
-        stride = compute_stride(
-            (input_shape[1], input_shape[2]),
-            (output_shape[1] // 2, output_shape[2] // 2),
-            2,
-        )
-        self.mp1 = nn.MaxPool2d(kernel_size=2, stride=stride)
-        stride = compute_stride(
-            (output_shape[1] // 2, output_shape[2] // 2),
-            (output_shape[1], output_shape[2]),
-            2,
-        )
-        self.mp2 = nn.MaxPool2d(kernel_size=2, stride=stride)
+        self.mp1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.mp2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.gelu = nn.GELU()
+        self.flatten = nn.Flatten()
 
     def forward(self, x_img: torch.Tensor) -> torch.Tensor:
         """
@@ -111,7 +105,7 @@ class FoveatedPerception(nn.Module):
         """
         x = self.gelu(self.mp1(self.conv1(x_img)))
         x = self.gelu(self.mp2(self.conv2(x)))
-        return x
+        return self.flatten(x)
 
 
 class PeripheralPerception(nn.Module):
@@ -123,12 +117,14 @@ class PeripheralPerception(nn.Module):
     It does so by using large convolutions with large stride.
     """
 
-    def __init__(self, input_shape, output_shape):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv1 = nn.Conv2d(input_shape[0], output_shape[0] // 2, 20, 4)
-        self.conv2 = nn.Conv2d(output_shape[0] // 2, output_shape[0], 10, 2)
-        self.max_pool = nn.MaxPool2d(2, 2)
+        self.conv1 = nn.Conv2d(in_channels, out_channels // 2, 40, 1)
+        self.conv2 = nn.Conv2d(out_channels // 2, out_channels, 20, 1)
+        self.mp1 = nn.MaxPool2d(5, 5)
+        self.mp2 = nn.MaxPool2d(5, 5)
         self.gelu = nn.GELU()
+        self.flatten = nn.Flatten()
 
     def forward(self, x_img: torch.Tensor) -> torch.Tensor:
         """
@@ -144,39 +140,7 @@ class PeripheralPerception(nn.Module):
         torch.Tensor
             Set of visual features (BS, -1, nH, nW)
         """
-        x = self.gelu(self.max_pool(self.conv1(x_img)))
-        x = self.gelu(self.max_pool(self.conv2(x)))
-        return x
+        x = self.gelu(self.mp1(self.conv1(x_img)))
+        x = self.gelu(self.mp2(self.conv2(x)))
+        return self.flatten(x)
 
-
-class VisualAttention(nn.Module):
-    def __init__(self, in_features):
-        super().__init__()
-        self.conv = nn.Conv2d(in_features, 1, 1)
-        self.softmax = nn.Softmax(dim=2)
-
-    def forward(self, fov_x: torch.Tensor, per_x: torch.Tensor) -> torch.Tensor:
-        """
-        Computation for combining the outputs of the other perception modules.
-        Performs attention over the concatenated result.
-
-        Parameters
-        ----------
-        fov_x : torch.Tensor
-            Output of the foveated perception module
-        per_x : torch.Tensor
-            Output of the peripheral perception module
-
-        Returns
-        -------
-        torch.Tensor
-            Fused feature representation
-        """
-        combined_features = torch.cat((fov_x, per_x), dim=1)
-        attention_features = self.conv(combined_features).view(
-            combined_features.size(0), 1, -1
-        )
-        attention_weights = self.softmax(attention_features)
-        flat_combined_features = combined_features.view(combined_features.size(0), -1)
-        x = (flat_combined_features * attention_weights).sum(dim=2)
-        return x
--- tests/perception/test_visual.py
+++ tests/perception/test_visual.py
@@ -5,21 +5,29 @@ from MineAI.perception.visual import (
     VisualPerception,
     FoveatedPerception,
     PeripheralPerception,
-    VisualAttention,
 )
+
+@pytest.fixture
+def visual_perception_module():
+    return VisualPerception()
+
+def test_visual_perception_forward(visual_perception_module):
+    input_tensor = torch.randn((32, 3, 160, 256))
+
+    output = visual_perception_module(input_tensor)
 
 
 @pytest.fixture
-def foveated_perception_model():
-    return FoveatedPerception()
+def foveated_perception_module():
+    return FoveatedPerception(3, 32)
 
 
-def test_foveated_perception_forward(foveated_perception_model):
+def test_foveated_perception_forward(foveated_perception_module):
     # Input tensor with shape (BS, 3, 160, 256)
     input_tensor = torch.randn((32, 3, 160, 256))
 
     # Forward pass
-    output = foveated_perception_model.forward(input_tensor)
+    output = foveated_perception_module.forward(input_tensor)
 
     # Check if the output has the expected shape
     assert output.shape[1] == 16  # Check the number of output channels
@@ -29,9 +37,9 @@ def test_foveated_perception_forward(foveated_perception_model):
     assert output.shape[3] == input_tensor.shape[3] // 4
 
 
-def test_foveated_perception_module_parameters(foveated_perception_model):
+def test_foveated_perception_module_parameters(foveated_perception_module):
     # Check the number of parameters in the module
-    num_params = sum(p.numel() for p in foveated_perception_model.parameters())
+    num_params = sum(p.numel() for p in foveated_perception_module.parameters())
 
     # Expected number of parameters can be calculated based on the convolution layers
     expected_params = 3 * (
@@ -39,3 +47,24 @@ def test_foveated_perception_module_parameters(foveated_perception_model):
     )  # 3 conv layers with specified parameters
 
     assert num_params == expected_params
+
+
+@pytest.fixture
+def peripheral_percetion_module():
+    return PeripheralPerception(1, 32)
+
+
+def test_peripheral_perception_forward(peripheral_perception_module):
+    # Input tensor with shape (BS, 3, 160, 256)
+    input_tensor = torch.randn((32, 3, 160, 256))
+
+    # Forward pass
+    output = peripheral_perception_module.forward(input_tensor)
+
+    # Check if the output has the expected shape
+    assert output.shape[1] == 16  # Check the number of output channels
+    assert (
+        output.shape[2] == input_tensor.shape[2] // 4
+    )  # Check the downsampling by MaxPool2d
+    assert output.shape[3] == input_tensor.shape[3] // 4
+
