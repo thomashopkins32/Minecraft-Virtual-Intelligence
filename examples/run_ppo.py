3dd09ed5ead8fa5049139c27ca5b20b570fc5e41
Thomas Hopkins <thomashopkins000@gmail.com>
2024-03-25 08:33:27 -0400

    Debugging torch indexing issue; Added unit test for PPO

--- examples/run_ppo.py
+++ examples/run_ppo.py
@@ -6,5 +6,5 @@ from mvi.learning.ppo import PPO
 if __name__ == "__main__":
     env = minedojo.make(task_id="open-ended", image_size=(160, 256))
     agent = AgentV1(env.action_space)
-    ppo = PPO(env, agent)
+    ppo = PPO(env, agent, steps_per_epoch=10)
     ppo.run()
