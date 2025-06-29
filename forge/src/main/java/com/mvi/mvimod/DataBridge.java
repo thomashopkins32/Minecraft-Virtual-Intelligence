ab36cb22d1305deedcbc8f44f7f21dafed7d4d21
Thomas Hopkins <thomashopkins000@gmail.com>
2025-06-29 13:42:12 -0400

    Solid first draft of communicating frame data over UDP

--- forge/src/main/java/com/mvi/mvimod/DataBridge.java
+++ forge/src/main/java/com/mvi/mvimod/DataBridge.java
@@ -1,29 +1,41 @@
 package com.mvi.mvimod;
 
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
 public class DataBridge {
+  private static final Logger LOGGER = LogUtils.getLogger();
   private static DataBridge instance;
   private NetworkHandler networkHandler;
 
   public static DataBridge getInstance() {
     if (instance == null) {
       instance = new DataBridge();
+      LOGGER.info("DataBridge instance created");
     }
     return instance;
   }
 
   public void setNetworkHandler(NetworkHandler handler) {
     this.networkHandler = handler;
+    LOGGER.info("NetworkHandler connected to DataBridge");
   }
 
   public void sendEvent(String eventType, String data) {
     if (networkHandler != null) {
+      LOGGER.info("Sending event: {} with data: {}", eventType, data);
       // TODO: Implement this
+    } else {
+      LOGGER.warn("Cannot send event - NetworkHandler is null");
     }
   }
 
   public void sendFrame(byte[] frameData) {
     if (networkHandler != null) {
+      LOGGER.info("DataBridge sending frame data (size: {} bytes)", frameData.length);
       networkHandler.sendResponse(frameData, 0);
+    } else {
+      LOGGER.warn("Cannot send frame - NetworkHandler is null");
     }
   }
 }
